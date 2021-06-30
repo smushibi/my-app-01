2e4771366d2f89163bbfdb5526b5745207bd5948
samutachika mushibi <samutachika@gmail.com>
2021-06-30 13:50:47 +0200

    useState

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState}from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  return (
+  const [name, setName ]
+= useState("Monk")
+const onChange =()=>{
+  setName("Chikiti")
+}
+return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textColor}>sepio is coding in react native </Text>
+      <View><Button  onPress={onChange}  title="change state"/></View>
     </View>
+    <View style={styles.container2}><Text>Monty python {name}</Text></View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 0.5,
+    backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textColor:{
+    color:"blue",
+    fontSize: 60,
+    
+  },
+  container2: {
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 0.5,
+    
+  },
+  
 });
