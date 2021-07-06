a3ad6685ec89f9b08b1e80f5ae3374b3c0471aa2
samutachika mushibi <samutachika@gmail.com>
2021-07-06 14:00:41 +0200

    FlatList

--- App.js
+++ App.js
@@ -1,67 +1,54 @@
 
 import React, {useState}from 'react';
-import { StyleSheet, Text, View, Button , TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button , FlatList} from 'react-native';
 
 export default function App() {
-  const [name, setName ]= useState('George')
   const [names, setNames ]= useState([{name:"white", id:1}, 
-                                      {name:"sepio", id:2 },
-                                      {name:"sepio", id:2 },
-                                      {name:"sepio", id:2 },
-                                      {name:'monk', id:3}])
+                                      {name:"sepioa", id:2 },
+                                      {name:"sepio1", id:3 },
+                                      {name:"sepio2", id:4 },
+                                      {name:"sepio3", id:5 },
+                                      {name:"sepio4", id:6 },
+                                      {name:"sepio5", id:7 },
+                                      {name:"sepio6", id:8 },
+                                      {name:"sepio7", id:9 },
+                                      {name:'monk7', id:10},])
 const onChange =()=>{
   setName("sepio")
 }
 return (
-    <>
-    <View style={styles.container}>
-      <Text style={styles.textColor}> react native </Text>
-      <View><Button  onPress={onChange}  title="change state"/></View>
-      <View>{names.map((name,id) =>(<View key={id}><Text>{name.name}</Text></View>))}</View>
+    <View  style={styles.container}>
+      <Text>Hello Coder</Text>
+      <FlatList
+      data={names}
+      renderItem={({item})=>(<View style={styles.container2}><Button   title={item.name}/></View>)}
+      keyExtractor={(item)=> item.id}
+      />
     </View>
-    <View style={styles.container2}><Text>my name is  {name}</Text></View>
-    <View style={styles.input}>
-      <Text >Enter data</Text>
-      <TextInput style={styles.textInput} 
-        placeholder="enter data here :)"
-        onChangeText={(val)=> setName(val)}
-       />
-      </View>
-    </>
+   
   );
 } 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 0.5,
-    backgroundColor: 'lightgreen',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textColor:{
-    color:"blue",
-    fontSize: 60,
-  },
-  container2: {
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 0.5,
-  },
-  textInput:{
-    borderColor:'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin:2,
-    borderWidth:1,
-   
-    padding:6,
 
-  },
-  input: {
-    flex: 0.5,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+container:{
+  backgroundColor:"yellow",
+  alignItems:"center"
+ 
+
+},
+text:{
+  
+  fontSize:30,
+  alignItems:'center'
+},
+container2:{
+  margin:15,
+  padding:10,
+  borderRadius:10,
+  backgroundColor:'lightblue',
+  width:120
+ 
+}
+
+})
